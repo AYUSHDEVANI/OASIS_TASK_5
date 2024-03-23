202ee01bd7db3421c938b20edc9ea66d23530a46
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-24 00:19:16 +0530

    Make a function for handle client connections.

--- Server_side.py
+++ Server_side.py
@@ -8,3 +8,12 @@ PORT = 8888
 
 # List to store client connections
 clients = []
+
+# Function to handle client connections
+def handle_client(client_socket, address):
+    while True:
+        message = client_socket.recv(1024).decode()
+        for client in clients:
+            if client != client_socket:
+                client.send(message.encode())
+
