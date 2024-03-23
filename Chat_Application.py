0da5d4a3df472e19d032409beb421bab5ace4eac
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-24 00:26:20 +0530

    Make a function for receive messages from server side.

--- Chat_Application.py
+++ Chat_Application.py
@@ -5,3 +5,12 @@ import threading
 # Client configuration
 HOST = "127.0.0.1"
 PORT = 8888
+
+
+# Function to receive messages from server 
+def receive_messages(client_socket):
+    while True:
+        message = client_socket.recv(1024).decode()
+        print(message)
+
+        
